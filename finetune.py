66181cce4cb181b3751a779a2ad9035956202401
Xinyu Feng <xinyuf@usc.edu>
2024-06-09 16:03:37 -0400

    sft script

---
+++ finetune.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from datasets import Dataset
+# Load model directly
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from trl import SFTConfig, SFTTrainer
+
+tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2-0.5B")
+model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2-0.5B")
+
+# Load your dataset
+df = pd.read_csv('train_set.csv')
+
+# Convert to Hugging Face Dataset
+dataset = Dataset.from_pandas(df)
+
+sft_config = SFTConfig(
+    dataset_text_field="text",
+    max_seq_length=512,
+    output_dir="/qwen2-vtb",
+)
+trainer = SFTTrainer(
+    model = model,
+    tokenizer = tokenizer,
+    train_dataset=dataset,
+    args=sft_config,
+)
+trainer.train()
