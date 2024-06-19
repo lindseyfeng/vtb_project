984b3527b40fc3afa492381bef134ce10c2d349c
Lindsey Feng <lindseyf@Lindseys-MacBook-Air.local>
2024-06-19 01:20:37 -0700

    dpo update

--- dpo.py
+++ dpo.py
@@ -25,96 +25,6 @@ from typing import List, Dict, Any, Optional
 import torch
 from torch.nn.utils.rnn import pad_sequence
 
-class DPODataCollatorWithPadding:
-    r"""
-    DPO DataCollator class that pads the tokenized inputs to the maximum length of the batch.
-    Args:
-        pad_token_id (`int` defaults to 0):
-            The tokenizer's pad_token_id.
-        label_pad_token_id (`int`, defaults to -100):
-            The label used for masking.
-        is_encoder_decoder (`Optional[bool]`, `optional`, defaults to `None`):
-            Whether or not you model has an encoder_decoder architecture.
-    """
-
-    def __init__(self, pad_token_id: int = 0, label_pad_token_id: int = -100, is_encoder_decoder: Optional[bool] = False):
-        self.pad_token_id = pad_token_id
-        self.label_pad_token_id = label_pad_token_id
-        self.is_encoder_decoder = is_encoder_decoder
-
-    def __call__(self, f: List[Dict[str, Any]]) -> Dict[str, Any]:
-        # Filter out any features that have None values
-        features = []
-        for feature in f:
-            if all(
-                isinstance(value, list) and all(item is not None for item in value)
-                for key, value in feature.items() if key.endswith("_input_ids") or key.endswith("_attention_mask") or key.endswith("_labels")
-            ):
-                features.append(feature)
-            else:
-                print(f"Invalid feature detected and ignored: {feature}")
-
-        if not features:
-            raise ValueError("No valid features provided after filtering out None values.")
-
-        # First, pad everything to the same length
-        padded_batch = {}
-        for k in features[0].keys():
-            if k.endswith("_input_ids") or k.endswith("_attention_mask") or k.endswith("_labels"):
-                if self.is_encoder_decoder:
-                    to_pad = [torch.LongTensor(ex[k]) for ex in features]
-
-                    if (k.startswith("prompt")) and (k.endswith("input_ids")):
-                        if self.pad_token_id is None:
-                            raise ValueError(
-                                "Padding is enabled, but the tokenizer is not configured with a padding token."
-                                " Explicitly set `tokenizer.pad_token` (e.g. `tokenizer.pad_token = tokenizer.eos_token`)"
-                                " before calling the trainer."
-                            )
-                        padding_value = self.pad_token_id
-                    elif k.endswith("_attention_mask"):
-                        padding_value = 0
-                    elif k.startswith(("chosen", "rejected", "completion")) or ("decoder" in k):
-                        padding_value = self.label_pad_token_id
-                    else:
-                        raise ValueError(f"Unexpected key in batch '{k}'")
-                    padded_batch[k] = pad_sequence(to_pad, batch_first=True, padding_value=padding_value)
-                else:
-                    # adapted from https://stackoverflow.com/questions/73256206
-                    if "prompt" in k:
-                        to_pad = [torch.LongTensor(ex[k][::-1]) for ex in features]
-                    else:
-                        for i in features:
-                            print(i)
-                        to_pad = [torch.LongTensor(ex[k]) for ex in features]
-                    if k.endswith("_input_ids"):
-                        if self.pad_token_id is None:
-                            raise ValueError(
-                                "Padding is enabled, but the tokenizer is not configured with a padding token."
-                                " Explicitly set `tokenizer.pad_token` (e.g. `tokenizer.pad_token = tokenizer.eos_token`)"
-                                " before calling the trainer."
-                            )
-                        padding_value = self.pad_token_id
-                    elif k.endswith("_labels"):
-                        padding_value = self.label_pad_token_id
-                    elif k.endswith("_attention_mask"):
-                        padding_value = 0
-                    else:
-                        raise ValueError(f"Unexpected key in batch '{k}'")
-
-                    padded_batch[k] = pad_sequence(to_pad, batch_first=True, padding_value=padding_value)
-                    # for the prompt, flip back so padding is on left side
-                    if "prompt" in k:
-                        padded_batch[k] = padded_batch[k].flip(dims=[1])
-            elif k.endswith("_logps"):
-                # the cached reference model logprobs
-                padded_batch[k] = torch.tensor([ex[k] for ex in features])
-            else:
-                padded_batch[k] = [ex[k] for ex in features]
-
-        return padded_batch
-
-
 
     
 # Define and parse arguments.
@@ -137,8 +47,8 @@ class ScriptArguments:
     weight_decay: Optional[float] = field(default=0.05, metadata={"help": "the weight decay"})
     optimizer_type: Optional[str] = field(default="rmsprop", metadata={"help": "the optimizer type"})
     num_train_epochs: Optional[int] = field(default=1, metadata={"help": "num epoch"})
-    per_device_train_batch_size: Optional[int] = field(default=32, metadata={"help": "train batch size per device"})
-    per_device_eval_batch_size: Optional[int] = field(default=32, metadata={"help": "eval batch size per device"})
+    per_device_train_batch_size: Optional[int] = field(default=16, metadata={"help": "train batch size per device"})
+    per_device_eval_batch_size: Optional[int] = field(default=16, metadata={"help": "eval batch size per device"})
     gradient_accumulation_steps: Optional[int] = field(
         default=4, metadata={"help": "the number of gradient accumulation steps"}
     )
@@ -321,7 +231,6 @@ if __name__ == "__main__":
         max_length=script_args.max_length,
         max_target_length=script_args.max_target_length,
         force_use_ref_model = True,
-        data_collator = DPODataCollatorWithPadding(tokenizer.pad_token_id, -100, model.config.is_encoder_decoder)
     )
 
     # 6. train
