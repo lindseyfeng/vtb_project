c34c714260724f57d1fb3012ac2b83d756522f83
Lindsey Feng <lindseyf@Lindseys-MacBook-Air.local>
2024-06-19 23:27:27 -0700

    dpo update

--- code/finetune.py
+++ code/finetune.py
@@ -4,11 +4,22 @@ from datasets import Dataset
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from trl import SFTConfig, SFTTrainer
 
+
+def formatting_prompts_func(example):
+    output_texts = []
+    for i in range(len(example['instruction'])):
+        text = f"### Question: {example['Prompt'][i]}\n ### Answer: {example['Chosen']}"
+        output_texts.append(text)
+    return output_texts
+
+
 tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2-0.5B")
 model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2-0.5B")
+response_template = " ### Answer:"
+collator = DataCollatorForCompletionOnlyLM(response_template, tokenizer=tokenizer)
 
 # Load your dataset
-df = pd.read_csv('train_set.csv')
+df = pd.read_csv('../train_set.csv')
 
 # Convert to Hugging Face Dataset
 dataset = Dataset.from_pandas(df)
@@ -19,12 +30,13 @@ sft_config = SFTConfig(
     max_seq_length=512,
     per_device_train_batch_size = 1,
     gradient_accumulation_steps = 8, 
-    output_dir="./qwen2-vtb",
+    output_dir="./qwen2-vtb-dpo-sft",
 )
 trainer = SFTTrainer(
-    model = model,
-    tokenizer = tokenizer,
+    model,
     train_dataset=dataset,
-    args=sft_config,
+    args=SFTConfig(output_dir="/tmp"),
+    formatting_func=formatting_prompts_func,
+    data_collator=collator,
 )
 trainer.train()
